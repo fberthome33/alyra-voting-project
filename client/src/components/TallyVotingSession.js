9842c271eedae07aa5b5527e767df537213b11ef
Fabien Berthomé <fabien.berthome.ext@veolia.com>
2022-04-24 18:09:51 +0200

    tally vote page for voter

--- client/src/components/TallyVotingSession.js
+++ client/src/components/TallyVotingSession.js
@@ -1,78 +1,96 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-
-export default class TallyVotingSession extends React.Component {
-
-    constructor(props) {
-        super(props);
+import { Proposal } from '../Proposal';
 
 
+function TallyVotingSession ({
+  state
+}) {
+const [stateProposal, setStateProposal] = useState({ listProposal: [], winningProposalID: state.winningProposalID,workflowStatus: state.workflowStatus });
 
-    }
 
-    setVote = async (e) => {
+  useEffect(() => {
+    (async function () {
+      try {
+        let options = {
+          fromBlock: 0,                  //Number || "earliest" || "pending" || "latest"
+          toBlock: 'latest'
+        };
+        const listProposalEvents = (await state.contract.getPastEvents('ProposalRegistered', options));
+        const listProposal = [];
+        listProposalEvents.forEach(async(indexProps) => 
+        { 
+          console.log(indexProps.returnValues.proposalId)
+          const proposal =  await state.contract.methods.getOneProposal(Number(indexProps.returnValues.proposalId)).call({ from: state.accounts[0]});
+          listProposal.push(new Proposal(Number(indexProps.returnValues.proposalId), proposal.description, proposal.voteCount ));
+          setStateProposal(s => ({...s, listProposal: listProposal}))
+          console.log(listProposal);
+        });
+      } catch (error) {
+        // Catch any errors for any of the above operations.
+        alert(
+          `Failed to init TallyVotingSession component.`,
+        );
+        console.error(error);
+      }
+    })();
+  }, [])
+
+
+    const tallyVotes = async (e) => {
       e.preventDefault();
-      const { accounts, contract } = this.props.state;
-      let newProposalDesc = this.props.state.proposalId.value;
-      await contract.methods.setVote(newProposalDesc).send({ from: accounts[0] });
-
-
-    }
-
-    tallyVotes = async (e) => {
-      e.preventDefault();
-      const { accounts, contract } = this.props.state;
-      //let newProposalDesc = this.props.state.proposalId.value;
+      const { accounts, contract } = state;
+      //let newProposalDesc = state.proposalId.value;
       await contract.methods.tallyVotes().send({ from: accounts[0] });
 
-      this.props.state.winningProposalID = Number(await contract.methods.winningProposalID().call());
-      this.props.state.workflowStatus  = Number(await contract.methods.workflowStatus().call());
+      state.winningProposalID = Number(await contract.methods.winningProposalID().call());
+      state.workflowStatus  = Number(await contract.methods.workflowStatus().call());
     }
 
-    render(){
+  
         return(
       <><div style={{ display: 'flex', justifyContent: 'center' }}>
 
             <Card style={{ width: '50rem' }}>
               <Card.Header>
-              { this.props.state.workflowStatus === 4 &&
+              { state.workflowStatus === 4 &&
                 <strong> Vote Not Tallied
                 </strong>
               }
-              { this.props.state.workflowStatus === 5 &&
+              { state.workflowStatus === 5 &&
                 <strong> The winner is 
                 </strong>
               }
               </Card.Header>
               <Card.Body>
-              { this.props.state.workflowStatus === 5 && this.props.state.voter &&
+              { state.workflowStatus === 5 && state.voter &&
                 <strong>
-                  id: {this.props.state.listProposal[this.props.state.winningProposalID]?.id}
+                  id: {stateProposal.listProposal[stateProposal.winningProposalID]?.id}
                   <br/>
-                  description: {this.props.state.listProposal[this.props.state.winningProposalID]?.description}
+                  description: {stateProposal.listProposal[stateProposal.winningProposalID]?.description}
                   <br/>
-                  voteCount: {this.props.state.listProposal[this.props.state.winningProposalID]?.voteCount}
+                  voteCount: {stateProposal.listProposal[stateProposal.winningProposalID]?.voteCount}
                 </strong>
               }
-              { this.props.state.workflowStatus === 5 && this.props.state.accounts && this.props.state.accounts[0] && this.props.state.accounts[0] === this.props.state.owner &&
+              { state.workflowStatus === 5 && state.accounts && state.accounts[0] && state.accounts[0] === state.owner &&
                 <strong>
-                  {this.props.state.winningProposalID}
+                  {stateProposal.winningProposalID}
                 </strong>
               }
               </Card.Body>
             </Card>
           </div><br></br>
-          { this.props.state.accounts && this.props.state.accounts[0] && this.props.state.accounts[0] === this.props.state.owner &&
-           this.props.state.workflowStatus === 4 && 
+          { state.accounts && state.accounts[0] && state.accounts[0] === state.owner &&
+           state.workflowStatus === 4 && 
             <div style={{ display: 'flex', justifyContent: 'center' }}>
               <Card style={{ width: '50rem' }}>
                 <Card.Header><strong>Tally Vote</strong></Card.Header>
                 <Card.Body>
                   <Form.Group controlId="formProposal">
                     <Form.Control type="text" id="proposal"
-                      ref={(input) => { this.props.state.proposalId = input; } } />
+                      ref={(input) => { state.proposalId = input; } } />
                   </Form.Group>
                   <Button onClick={this.tallyVotes} variant="dark"> Tally Vote </Button>
                 </Card.Body>
@@ -80,6 +98,5 @@ export default class TallyVotingSession extends React.Component {
             </div>
           }</>
         )
-    }
-
 }
+export default TallyVotingSession;
